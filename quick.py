4ff45a0ff78dd6abfcd4bc3920b268a3362515e0
Mike O'Shea <moshea@paraport.com>
2024-02-21 11:05:06 -0800

    comments and import cleanup

--- quick.py
+++ quick.py
@@ -1,8 +1,9 @@
-# diagram.py
-from diagrams import Diagram, Cluster, Edge
-from diagrams.aws.compute import EC2, Lambda
-from diagrams.aws.database import RDS, Dynamodb, ElastiCache
-from diagrams.aws.network import ELB
+# diagram example
+# https://diagrams.mingrammer.com/docs/guides/diagram
+
+from diagrams import Diagram, Cluster
+from diagrams.aws.compute import Lambda
+from diagrams.aws.database import Dynamodb, ElastiCache
 from diagrams.aws.integration import StepFunctions
 from diagrams.azure.database import SQLDatabases
 from diagrams.programming.framework import GraphQL
@@ -32,32 +33,4 @@ with Diagram("PCW Data Integration", show=False):
     onpremSql >> createSession
     integration >> createSession
     integration >> Lambda("PCW Account List")
-
-
-# list the members of the diagrams.aws.compute module
-# print(dir(diagrams.aws.compute))
-# ['ASG', 'AutoScaling', 'EC2', 'ECS', 'EKS', 'Lambda', 'LightSail', 'VM']
-# list the members of the diagrams.aws.database module
-# print(dir(diagrams.aws.database))
-# ['DynamoDB', 'ElasticCache', 'Neptune', 'RDS', 'Redshift']
-# list the members of the diagrams.aws.network module
-# print(dir(diagrams.aws.network))
-# ['APIGateway', 'CloudFront', 'CloudMap', 'DirectConnect', 'ELB', 'GlobalAccelerator', 'Route53', 'VPC', 'VPCPeering', 'VPCPrivate', 'VPCPublic']
-
-# list the members of the diagrams.aws module
-# print(dir(diagrams.aws))
-# ['analytics', 'applicationintegration', 'compute', 'database', 'developer', 'enduser', 
-#'engagement', 'iot', 'management', 'media', 'migration', 'mobile', 'network', 'security', 'storage', 'web']
-
-# Use boto3 to list the APIs in APIGateway
-# import boto3
-# client = boto3.client('apigateway')
-# response = client.get_rest_apis()
-# for item in response['items']:
-#     print(item['name'])
-
-# Use boto3 to list the APIs in EC2 that have a specific tag WEB
-# import boto3
-# client = boto3.client('ec2')
-# response = client.describe_instances(
 
